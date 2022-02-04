2675371da05f10e73349b056d11de952c6ba8f8a
molodoypatsan <ledovskihdima2003@gmail.com>
2022-02-05 03:06:49 +0700

    fix

--- js/main.js
+++ js/main.js
@@ -14,3 +14,21 @@ new Swiper('.popular-excursions__slider', {
     },
     spaceBetween: 20,
 });
+
+new Swiper('.reviews__slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+    },
+
+    autoplay: {
+        disableOnInteraction: false,
+    },
+    spaceBetween: 20,
+    sliderPerGroup: 2
+});
